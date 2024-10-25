1817ab0dee70321a7c5be97a765b92aa59e94960
ivongr <itzelivongr@gmail.com>
2024-10-24 20:00:36 -0600

    update: agrega useEffect a showAlert

--- src/showAlert.tsx
+++ src/showAlert.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
+import { useEffect } from 'react';
 
-interface AlertProps{
+interface AlertProps {
   message: string,
   setShowAlert: (value: boolean) => void;
 }
 
 const ShowAlert: React.FC<AlertProps> = ({ message, setShowAlert }) => {
-  
 
+  useEffect(() => {
     const time = setTimeout(() => {
       setShowAlert(false); // Oculta la alerta
     }, 5000);
+
+    return () => clearTimeout(time);
+  }, [setShowAlert]);
 
   return (
     <div className="alert" role="alert">
