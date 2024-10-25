9653da52a24e5a2fa0f30b60fef3467ba39beefd
Abel Herrera <ahpard97@gmail.com>
2024-10-25 15:39:33 -0600

    chore: rename function to getUsers

--- src/listUsers.ts
+++ src/listUsers.ts
@@ -1,59 +1,58 @@
 const usersjson = JSON.stringify([
-    {
-        "name": "Suki Zukaritas",
-        "age": 5,
-        "address": {
-            "city": "Mtz",
-            "state": "Ver",
-            "zip": 4558
-        },
-        "email": "suki@gmail.com",
-        "password": "12345"
+  {
+    name: 'Suki Zukaritas',
+    age: 5,
+    address: {
+      city: 'Mtz',
+      state: 'Ver',
+      zip: 4558,
     },
-    {
-        "name": "Sandra Lopez",
-        "age": 25,
-        "address": {
-            "city": "Xalapa",
-            "state": "Ver",
-            "zip": 2545
-        },
-        "email": "sandra@gmail.com",
-        "password": "s123andr@"
+    email: 'suki@gmail.com',
+    password: '12345',
+  },
+  {
+    name: 'Sandra Lopez',
+    age: 25,
+    address: {
+      city: 'Xalapa',
+      state: 'Ver',
+      zip: 2545,
     },
-    {
-        "name": "Carlos Pérez",
-        "age": 30,
-        "address": {
-            "city": "Veracruz",
-            "state": "Ver",
-            "zip": 91500
-        },
-        "email": "carlos.perez@gmail.com",
-        "password": "carlosp123"
+    email: 'sandra@gmail.com',
+    password: 's123andr@',
+  },
+  {
+    name: 'Carlos Pérez',
+    age: 30,
+    address: {
+      city: 'Veracruz',
+      state: 'Ver',
+      zip: 91500,
     },
-    {
-        "name": "María González",
-        "age": 28,
-        "address": {
-            "city": "Coatepec",
-            "state": "Ver",
-            "zip": 91000
-        },
-        "email": "maria.gonzalez@gmail.com",
-        "password": "mariag2024"
-    }
+    email: 'carlos.perez@gmail.com',
+    password: 'carlosp123',
+  },
+  {
+    name: 'María González',
+    age: 28,
+    address: {
+      city: 'Coatepec',
+      state: 'Ver',
+      zip: 91000,
+    },
+    email: 'maria.gonzalez@gmail.com',
+    password: 'mariag2024',
+  },
 ]);
 
-
-export const listUsers = () => {
+export const getUsers = () => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       try {
         const users = JSON.parse(usersjson);
         resolve(users);
       } catch (error) {
-        reject(new Error("Error al cargar los usuarios."));
+        reject(new Error('Error al cargar los usuarios.'));
       }
     }, 1000);
   });
--- src/loginSession.ts
+++ src/loginSession.ts
@@ -1,19 +1,17 @@
-import { listUsers } from "./listUsers.js";
+import { getUsers } from './listUsers.js';
 
-
-async function loginSession(email:string, password:string) {
+async function loginSession(email: string, password: string) {
   try {
     const formattedEmail = email.toLowerCase();
-    const users:any = await listUsers();
-    return users.some((user:any) =>
-      user.email.toLowerCase() === formattedEmail
-      && user.password === password);
-  } catch (error:any) {
-    alert(error.message)
+    const users: any = await getUsers();
+    return users.some(
+      (user: any) => user.email.toLowerCase() === formattedEmail && user.password === password
+    );
+  } catch (error: any) {
+    alert(error.message);
   } finally {
-    console.log("Proceso finalizado")
+    console.log('Proceso finalizado');
   }
 }
-
 
 export { loginSession };
