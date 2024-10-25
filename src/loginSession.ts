873977cb143ffa93d749dc4e115f8d0ca7975134
ivongr <itzelivongr@gmail.com>
2024-10-25 09:32:00 -0600

    update: modifica el tipado de loginSession

--- src/loginSession.ts
+++ src/loginSession.ts
@@ -5,10 +5,10 @@ async function loginSession(email:string, password:string) {
   try {
     const formattedEmail = email.toLowerCase();
     const users:any = await listUsers();
-    return users.some(user =>
+    return users.some((user:any) =>
       user.email.toLowerCase() === formattedEmail
       && user.password === password);
-  } catch (error) {
+  } catch (error:any) {
     alert(error.message)
   } finally {
     console.log("Proceso finalizado")
