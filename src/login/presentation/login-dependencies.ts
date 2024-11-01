228f4a3016bf243d4a494ac66b6c3e3bb63e5ee4
Abel Herrera <ahpard97@gmail.com>
2024-11-01 15:02:43 -0600

    chore: add dependencies export and use sample

--- src/login/presentation/login-dependencies.ts
+++ src/login/presentation/login-dependencies.ts
@@ -1,26 +1,31 @@
-import { DIContainer } from "../../presentation/di-container";
-import { validateEmail } from "../domain/validation/validate-email";
-import { validatePassword } from "../domain/validation/validate-password";
-import { loginSession } from "../domain/session-login";
-import { dataSession } from "../domain/data-session";
-import { encryptValue } from "../domain/encrypt-value";
-import { expiresDateFormatIso, expiresDateLocal } from "../domain/expires-date";
+import { DIContainer } from '../../presentation/di-container';
+import { validateEmail } from '../domain/validation/validate-email';
+import { validatePassword } from '../domain/validation/validate-password';
+import { loginSession } from '../domain/session-login';
+import { dataSession } from '../domain/data-session';
+import { encryptValue } from '../domain/encrypt-value';
+import { expiresDateFormatIso, expiresDateLocal } from '../domain/expires-date';
 
 const container = new DIContainer();
 
-container.registerSingletone(() => validateEmail, "validateEmail");
+container.registerSingletone(() => validateEmail, 'validateEmail');
 
-container.registerSingletone(() => validatePassword, "validatePassword");
+container.registerSingletone(() => validatePassword, 'validatePassword');
 
-container.registerSingletone(() => loginSession, "loginSession");
+container.registerSingletone(() => loginSession, 'loginSession');
 
-container.registerSingletone(() => dataSession, "dataSession");
+container.registerSingletone(() => dataSession, 'dataSession');
 
-container.registerSingletone(() => expiresDateFormatIso, "expires-format-iso");
+container.registerSingletone(() => expiresDateFormatIso, 'expires-format-iso');
 
-container.registerSingletone(() => expiresDateLocal, "expires-local");
+container.registerSingletone(() => expiresDateLocal, 'expires-local');
 
-container.registerSingletone(() => encryptValue, "encrypt-value");
+container.registerSingletone(() => encryptValue, 'encrypt-value');
 
+export { container };
 
-export { container}
+// export const loginDependencies = {
+//   get service(): ILoginService {
+//     return container.get(keys.service);
+//   },
+// };
--- src/login/view/login-form.tsx
+++ src/login/view/login-form.tsx
@@ -1,37 +1,42 @@
-import { ILogin } from "../domain/login";
+import { ILogin } from '../domain/login';
 
-function LoginForm ({ email, setEmail, password, setPassword,onSubmit}: ILogin) {
+// function loginUser() {
+//   loginDependencies.service.login({ user: '', password: '' }).then(() => {
+//     redirectToLoginPage();
+//   });
+// }
+function LoginForm({ email, setEmail, password, setPassword, onSubmit }: ILogin) {
   return (
-    <form id="login-form" onSubmit={onSubmit}>
-      <h1 className="title">Login</h1>
+    <form id='login-form' onSubmit={onSubmit}>
+      <h1 className='title'>Login</h1>
       <label>
-        <i className="fa-solid fa-envelope" aria-label="Icon Email"></i>
+        <i className='fa-solid fa-envelope' aria-label='Icon Email'></i>
         <input
-          type="email"
-          id="email"
-          placeholder="Ej: SukiZukaritas@gmail.com"
+          type='email'
+          id='email'
+          placeholder='Ej: SukiZukaritas@gmail.com'
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           required
         />
       </label>
       <label>
-        <i className="fa-solid fa-lock"></i>
+        <i className='fa-solid fa-lock'></i>
         <input
-          type="password"
-          id="password"
-          aria-label="Icon Password"
-          placeholder="Password"
+          type='password'
+          id='password'
+          aria-label='Icon Password'
+          placeholder='Password'
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           required
         />
       </label>
-      <button id="btnlogin" className="btn success pulse-effect shadow-effect" type="submit" >
+      <button id='btnlogin' className='btn success pulse-effect shadow-effect' type='submit'>
         Iniciar Sesión
       </button>
     </form>
   );
-};
+}
 
 export default LoginForm;
