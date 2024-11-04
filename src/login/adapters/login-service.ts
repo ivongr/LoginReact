75abc2285fc4ffe03751e1f23e152cd2a074d195
ivongr <itzelivongr@gmail.com>
2024-11-04 15:36:28 -0600

    update: agrega parseLogin... para evitar unknown

--- src/login/adapters/login-service.ts
+++ src/login/adapters/login-service.ts
@@ -2,7 +2,7 @@ import { ILogin } from "../domain/entities/login";
 import { ILoginDataSource } from "../domain/entities/login-data-source";
 import { ILoginParams } from "../domain/entities/login-params";
 import { ILoginService } from "../domain/entities/login-service";
-import { parseLoginParams } from "../domain/validations/login-validations";
+import { parseLoginParams, parseLoginResponse } from "../domain/validations/login-validations";
 
 export class LoginService implements ILoginService {
 constructor(private readonly dataSource: ILoginDataSource) {}
@@ -15,8 +15,9 @@ async get(params?: ILoginParams): Promise<ILogin[]>{
     }
 
     const resp = await this.dataSource.get(parsedParams);
+    const parsedResp = parseLoginResponse(resp)
 
-    return resp.data;
+    return parsedResp.data;
 }
 
 }
