5c27f26df2465ae34996b0a92e008cd873886459
ivongr <itzelivongr@gmail.com>
2024-11-04 12:14:21 -0600

    update: actualiza la implementacion de las dependencias con loginService

--- src/login/presentation/login-dependencies.ts
+++ src/login/presentation/login-dependencies.ts
@@ -1,40 +1,28 @@
-import { DIContainer } from '../../infrastructure/di-container';
-import { validateEmail } from '../domain/validations/validate-email';
-import { validatePassword } from '../domain/validations/validate-password';
-import { loginSession } from '../domain/session-login';
-import { dataSession } from '../../domain/data-session';
-import { encryptValue } from '../../domain/encrypt-value';
-import { expiresDateFormatIso, expiresDateLocal } from '../domain/expires-date';
+import { DIContainer } from '../../shared/infrastructure/di-container';
+import { ILoginService } from '../domain/entities/login-service';
+import { ILoginDataSource } from '../domain/entities/login-data-source';
+import { LoginService } from '../adapters/login-service';
+
+
+
+const keys = {
+    service: 'service',
+    dataSource: 'data-source',
+};
 
 const container = new DIContainer();
 
-const keys = {validateEmail:'validateEmail',
-    validatePassword:'validatePassword',
-    loginSession:'loginSession',
-    dataSession:'dataSession',
-    expiresDateFormatIso:'expires-format-iso',
-    expiresDateLocal:'expires-local',
-    encryptValue: 'encrypt-value'
-};
+container.registerSingletone(createLoginService, keys.service)
 
-container.registerSingletone(() => validateEmail, keys.validateEmail);
 
-container.registerSingletone(() => validatePassword, keys.validatePassword);
+function createLoginService() {
+    const dataSource = container.get<ILoginDataSource>(keys.dataSource);
+    return new LoginService(dataSource)
+}
+ export const loginDependencies = {
+  get service(): ILoginService {
+    return container.get(keys.service);
+  },
+}; //get para evitar importar varios archivos 
 
-container.registerSingletone(() => loginSession, keys.loginSession);
 
-container.registerSingletone(() => dataSession, keys.dataSession);
-
-container.registerSingletone(() => expiresDateFormatIso, keys.expiresDateFormatIso);
-
-container.registerSingletone(() => expiresDateLocal, keys.expiresDateLocal);
-
-container.registerSingletone(() => encryptValue, keys.encryptValue);
-
-export { container};
-
-// export const loginDependencies = {
-//   get service(): ILoginService {
-//     return container.get(keys.service);
-//   },
-// }; //get para evitar importar varios archivos 
