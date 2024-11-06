a5edf80b4346e1a57036ab7e4d951445484327fe
ivongr <itzelivongr@gmail.com>
2024-11-06 16:13:12 -0600

    feat: agrega un nuevo esquema de validacion

---
+++ src/login/domain/validations/validate-session-storage-schema.ts
@@ -0,0 +1,7 @@
+import {  date, number, object,string } from "valibot";
+
+export const validateSessionStorageSchema = object({
+    email: string(),
+    password: string(),
+    date: string(),
+  })
