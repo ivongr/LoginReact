71adaf90659d42c93acaf1a7422b09a887b46d87
ivongr <itzelivongr@gmail.com>
2024-11-11 15:27:58 -0600

    refactor: separa la validacion en archivos diferentes

---
+++ src/login/domain/constants/session-store-error-messages.ts
@@ -0,0 +1,21 @@
+
+const EXPIRATION_DATE_ERROR_MESSAGES = {
+    required: 'Fecha requerida',
+    invalidFormat: 'Formato incorrecto'
+}
+
+
+
+const SESSION_ERROR_MESSAGES = {
+
+    credentials:{
+        email: {
+            required: 'El email es requerido'
+        },
+        password: {
+            required: 'El password es requerido'
+        }
+    }
+}
+
+export {EXPIRATION_DATE_ERROR_MESSAGES, SESSION_ERROR_MESSAGES}
--- src/login/domain/validations/session-store-validations.ts
+++ src/login/domain/validations/session-store-validations.ts
@@ -1,18 +1,19 @@
-import { isoTimestamp, object, parse, pipe, string, transform } from 'valibot';
+import { date, isoTimestamp, object, parse, pipe, string, transform } from 'valibot';
 
 import { ISessionStoreData } from '../entities/session-store-data';
+import { EXPIRATION_DATE_ERROR_MESSAGES, SESSION_ERROR_MESSAGES } from '../constants/session-store-error-messages';
 
 
 const expirationDateSchema = pipe(
-  string(),
-  isoTimestamp("formato incorrecto"),
+  string(EXPIRATION_DATE_ERROR_MESSAGES.required),
+  isoTimestamp(EXPIRATION_DATE_ERROR_MESSAGES.invalidFormat),
   transform(expirationDate => new Date(expirationDate))
 );
 
 export const sessionStoreDataSchema = object({
   credentials: object({
-    email: string(),
-    password: string(),
+    email: string(SESSION_ERROR_MESSAGES.credentials.email.required),
+    password: string(SESSION_ERROR_MESSAGES.credentials.password.required),
   }),
   expirationDate: expirationDateSchema,
 });
@@ -22,9 +23,17 @@ export const sessionStoreDataSchema = object({
   email: "suki@gmail.com",
   password: "12345"
 },
-  expirationDate: "2024-11-11T17:32:31.086Z"});
+  expirationDate: "2024-11-11T18:23:41.581Z"});
 console.log(result);*/
 
+
+export const sessionStoreDataParamsSchema = object({
+  credentials: object ({
+    email: string(),
+    password: string(),
+  }),
+  expirationDate: date()
+});
 
 /*Parsear objecto para que no de unknown*/
 export function parseSessionStoreData(data: unknown): ISessionStoreData {
